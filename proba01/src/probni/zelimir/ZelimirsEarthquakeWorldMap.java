f4eeabeeb391a6a53f7115eb39036855ab9ae5ae
zelimir stojcevic <zelimir.s.bl@gmail.com>
2017-12-09 18:11:33 +0100

    map key adjusted

--- proba01/src/probni/zelimir/ZelimirsEarthquakeWorldMap.java
+++ proba01/src/probni/zelimir/ZelimirsEarthquakeWorldMap.java
@@ -9,8 +9,7 @@ import de.fhpotsdam.unfolding.UnfoldingMap;
 import de.fhpotsdam.unfolding.data.Feature;
 import de.fhpotsdam.unfolding.marker.AbstractMarker;
 import de.fhpotsdam.unfolding.marker.Marker;
-import de.fhpotsdam.unfolding.marker.SimplePointMarker;
-import de.fhpotsdam.unfolding.providers.EsriProvider;
+import de.fhpotsdam.unfolding.providers.Microsoft;
 import de.fhpotsdam.unfolding.utils.MapUtils;
 import processing.core.PApplet;
 import processing.core.PFont;
@@ -39,15 +38,12 @@ public class ZelimirsEarthquakeWorldMap extends PApplet {
 		// ZelimirParser.loadFeaturesFromRSS(this, rssQuakeUrlOffline);
 		// earthquakeMarkers = new
 		// ArrayList<>();/////////////////////////////////////////////////////////////////////
-//		 ImageMarker imgMarker = null;
 
 		size(1200, 600, OPENGL);
 
-		// map = new UnfoldingMap(this, 200, 50, 900, 500, new
-		// Microsoft.RoadProvider());
-		map = new UnfoldingMap(this, 200, 50, 900, 500, new EsriProvider.DeLorme());
-		// map = new UnfoldingMap(this, 200, 50, 900, 500, new
-		// Google.GoogleMapProvider());
+		 map = new UnfoldingMap(this, 200, 50, 900, 500, new  Microsoft.RoadProvider());
+//		map = new UnfoldingMap(this, 200, 50, 900, 500, new EsriProvider.DeLorme());
+//		 map = new UnfoldingMap(this, 200, 50, 900, 500, new Google.GoogleMapProvider());
 
 		map.zoomToLevel(2);
 		MapUtils.createDefaultEventDispatcher(this, map);
@@ -83,7 +79,6 @@ public class ZelimirsEarthquakeWorldMap extends PApplet {
 		for (Marker mk : earthquakeMarkers) {
 			System.out.println("### " + mk.getProperties());
 		}
-		System.out.println("W *** " + map.getWidth() + "   H *** " + map.getHeight());
 	}
 
 	public void draw() {
@@ -125,8 +120,9 @@ public class ZelimirsEarthquakeWorldMap extends PApplet {
 		fill(255, 255, 255);
 		text(str, 30, 60, 180, 80);
 
-		fill(255, 0, 0);
-		ellipse(40, 130, 16, 16);
+//		fill(255, 0, 0);
+//		ellipse(40, 130, 16, 16);
+		image(loadImage("data/ui/marker_red.png"), 40-11, 100);
 
 		String stri = "5 + magnitude";
 		textFont(f01);
@@ -135,8 +131,9 @@ public class ZelimirsEarthquakeWorldMap extends PApplet {
 		fill(255, 255, 255);
 		text(stri, 58, 122, 180, 80);
 
-		fill(255, 247, 0);
-		ellipse(40, 180, 10, 10);
+//		fill(255, 247, 0);
+//		ellipse(40, 180, 10, 10);
+		image(loadImage("data/ui/marker_yellow.png"), 40-11, 130+20);
 
 		String strin = "(4, 4.9] magnitude";
 		textFont(f01);
@@ -144,14 +141,23 @@ public class ZelimirsEarthquakeWorldMap extends PApplet {
 		fill(255, 255, 255);
 		text(strin, 58, 172, 180, 80);
 
-		fill(0, 191, 255);
-		ellipse(40, 230, 5, 5);
+//		fill(0, 191, 255);
+//		ellipse(40, 230, 5, 5);
+		image(loadImage("data/ui/marker_gray.png"), 40-11, 130+70);
 
-		String string = "- 4 magnitude";
+		String string = "4 - magnitude";
 		textFont(f01);
 		textSize(14);
 		fill(255, 255, 255);
 		text(string, 58, 222, 180, 80);
+		
+		fill(255, 0, 0);
+		ellipse(42, 270, 15, 15);
+		String sss = "Past Hour";
+		textFont(f01);
+		textSize(14);
+		fill(255, 255, 255);
+		text(sss, 58, 265, 180, 80);
 	}
 
 }
